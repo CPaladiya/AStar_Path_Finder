969ec4aad1cfbc2753e97e6b7f06f3396e163f52
cpaladiya <chirag.paladiya@hotmail.com>
2021-07-07 14:29:56 -0400

    Feat : Add and Fix 2D Button Grid

--- LOSLogic.cpp
+++ LOSLogic.cpp
@@ -8,17 +8,24 @@ void Window::LoadWindow(){
     
     MainWindowGrid = new QGridLayout;
 
-    // -------------- adding 2D grid 200 buttons---------------//
+    // -------------- adding 2D grid 200 buttons to 2DGrid box---------------//
 
     QGroupBox *TwoDGridButtonBox = new QGroupBox;
     QGridLayout *TwoDGridButtonGridLayout = new QGridLayout;
 
-
-    QPushButton* test = new QPushButton;
-    TwoDGridButtonGridLayout->addWidget(test);
+    const QSize FixedButtonSize = QSize(20,20); //fixing the button size
+    //adding the many button to the layout
+    for(int i = 0; i<20 ; i++){
+        for(int j = 0 ; j<30; j++){
+            QPushButton *TempButton = new QPushButton();
+            TempButton->setFixedSize(FixedButtonSize); //fixing the button size before adding it
+            TwoDGridButtonGridLayout->addWidget(TempButton,i,j);
+        }
+    }
+    TwoDGridButtonGridLayout->setSpacing(0);
     TwoDGridButtonBox->setLayout(TwoDGridButtonGridLayout);
 
-    // --------------- adding A-Star and LOS button-------------//
+    // --------------- adding A-Star and LOS button to box below to 2D grid box-------------//
 
     QGroupBox *SimulateButtonBox = new QGroupBox;
     QGridLayout *SimulateButtonGrid = new QGridLayout;
@@ -33,13 +40,13 @@ void Window::LoadWindow(){
 
     //-----------adding both layout to main window--------//
 
-    MainWindowGrid->addWidget(TwoDGridButtonBox,0,0);
-    MainWindowGrid->addWidget(SimulateButtonBox,1,0);
+    MainWindowGrid->addWidget(TwoDGridButtonBox,0,0,10,1);
+    MainWindowGrid->addWidget(SimulateButtonBox,10,0,1,1);
 
     //----------Adding main window grid to QWidget--------//
     //Setting the grid, its title and Size of the window
-    setLayout(MainWindowGrid);
+    setLayout(MainWindowGrid); //Addimg main grid to the window
     setWindowTitle(tr("LOS Algorithm"));
-    resize(1000,600);
+    resize(100,200);
 
 }
