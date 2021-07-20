3de642e38a3f3fd47a2c10ede513aeab5fd09c1d
cpaladiya <chirag.paladiya@hotmail.com>
2021-07-20 15:38:58 -0400

    Chore : Comment Update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "Window.h"
 
 //This is the main file that starts the instance of window. It contains members of all the other class
+//it will hand over control to the qt module.
 
 int main(int argc, char *argv[])
 {
